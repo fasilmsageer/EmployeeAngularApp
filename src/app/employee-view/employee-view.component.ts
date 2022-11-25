4bc601e37a7c305d00edb7856654dd3115aec215
fasilmsageer <fasilmsageer@gmail.com>
2022-11-25 11:02:01 +0530

    databinding

--- src/app/employee-view/employee-view.component.ts
+++ src/app/employee-view/employee-view.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-view.component.css']
 })
 export class EmployeeViewComponent {
+  data = [
+    {
+      "name":"phill",
+      "designation":"Engineer Trainee",
+      "salary": "4.5LPA",
+      "email":"phill@mail.com",
+      "EmployeeId":45878
+    },
+    {
+      "name":"luke",
+      "designation":"Engineer Trainee",
+      "salary": "4.5LPA",
+      "email":"luke@mail.com",
+      "EmployeeId":45877
+    },
+    {
+      "name":"haley",
+      "designation":"Engineer Trainee",
+      "salary": "4.5LPA",
+      "email":"haley@mail.com",
+      "EmployeeId":45876
+    },
+    {
+      "name":"alex",
+      "designation":"Engineer Trainee",
+      "salary": "4.5LPA",
+      "email":"alex@mail.com",
+      "EmployeeId":45875
+    },
+    {
+      "name":"phill",
+      "designation":"Engineer Trainee",
+      "salary": "4.5LPA",
+      "email":"phill@mail.com",
+      "EmployeeId":45878
+    },
+    {
+      "name":"phill",
+      "designation":"Engineer Trainee",
+      "salary": "4.5LPA",
+      "email":"phill@mail.com",
+      "EmployeeId":45878
+    }
+  ]
+  
+  
 
 }
